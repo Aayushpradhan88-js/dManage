2de702c51ea6347431dadb94cc4539f7247f13ce
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-28 15:31:41 +0545

    Writing Mailservice code and exported

--- backend/src/modules/global/services/nodeMailer.ts
+++ backend/src/modules/global/services/nodeMailer.ts
@@ -1,10 +1,12 @@
 import { Request } from "express"
-import "nodemailer" from "nodemailer"
+import nodemailer from "nodemailer"
 
 class MailService{
     static async sendMail(req:Request, res:Response){
-        const transporter = nodemailer
+        const transporter = nodemailer.createTransport({
+
+        })
     }
 }
 
-export default
+export default MailService
