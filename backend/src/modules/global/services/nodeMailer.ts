9d3f26fb8fec0cc6d8f03f549d37fef9dd10ef16
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-28 15:41:02 +0545

    mail service code completed

--- backend/src/modules/global/services/nodeMailer.ts
+++ backend/src/modules/global/services/nodeMailer.ts
@@ -1,12 +1,28 @@
-import { Request } from "express"
 import nodemailer from "nodemailer"
+import { NODEMAILER_GMAIL, NODEMAILER_GMAIL_APP_PASSWORD } from "../../../config/env"
+import GlobalErrorHandler from "./asyncErrorHandler";
 
-class MailService{
-    static async sendMail(req:Request, res:Response){
-        const transporter = nodemailer.createTransport({
+class MailService {
+    static sendMail = GlobalErrorHandler.asyncErrorHandler(
+        async () => {
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: NODEMAILER_GMAIL,
+                    pass: NODEMAILER_GMAIL_APP_PASSWORD
+                }
+            });
 
-        })
-    }
-}
+            const mailFormatObject = ({
+                from: "Ed-Tech <aayushpradhan789@gmail.com> ",
+                to: "",
+                subject: "",
+                text: ""
+            });
 
-export default MailService
+            await transporter.sendMail(mailFormatObject);
+        }
+    );
+};
+
+export default MailService;
