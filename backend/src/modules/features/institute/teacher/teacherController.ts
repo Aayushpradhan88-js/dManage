219237f27b8aea1ba6e0209a7cba9a39da8ed803
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-29 09:33:11 +0545

    comment out

--- backend/src/modules/features/institute/teacher/teacherController.ts
+++ backend/src/modules/features/institute/teacher/teacherController.ts
@@ -67,14 +67,14 @@ class TeacherController {
             }
         );
 
-        console.log("✅teacher email triggered")
+        // console.log("✅teacher email triggered")
         const mailInformation = {
             to: teacherEmail,
             subject: "Welcome to Software Development Course",
             text: `Here is you're email: ${teacherEmail} & password: ${(await passwordData).plainPassword}`
         };
 
-        console.log("✅nodemailer module triggered")
+        // console.log("✅nodemailer module triggered")
         try {
             await MailService.sendMail(mailInformation);
             return res.status(200).json({
