a8889e9fbca7a8647a179e8bd90c7ad1ab26f125
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-12-31 13:31:55 +0545

    make a debug file

--- backend/src/modules/features/teacher/teacherController.ts
+++ backend/src/modules/features/teacher/teacherController.ts
@@ -1,10 +1,10 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import IExtendedRequest from "../../global/types/types";
 import sequelize from "../../../database/connection";
 import { QueryTypes } from "sequelize";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
-import TokenGenerationService from "../../global/services/generateToken";
+// import TokenGenerationService from "../../global/services/generateToken";
 import { JWT_EXPIRY, JWT_SECRET } from "../../../config/env";
 
 interface ITeacherPassword {
@@ -13,7 +13,7 @@ interface ITeacherPassword {
 }
 
 class TeacherController {
-    static async teacherLogin(req: IExtendedRequest, res: Response) {
+    static async teacherLogin(req: Request, res: Response) {
         const { teacherInstituteNumber, teacherEmail, teacherPassword } = req.body;
         if (!teacherInstituteNumber || !teacherEmail || !teacherPassword) {
             return res.status(402).json({
@@ -22,13 +22,16 @@ class TeacherController {
             });
         };
 
-        const teacherData: ITeacherPassword[] = await sequelize.query(`SELECT * FROM teacher_${teacherInstituteNumber} WHERE teacherEmail=?`,
+        const teacherData: ITeacherPassword[] = await sequelize.query(`
+            SELECT * FROM teacher_${teacherInstituteNumber} WHERE teacherEmail=?`,
             {
                 type: QueryTypes.SELECT,
                 replacements: [teacherEmail]
             }
         );
-        console.log(teacherData, "teacherData");
+        console.log(teacherData[0]?.teacherPassword, "teacherData");
+        console.log(teacherData[0]?.id, "teacherData");
+        // console.log(teacherData, "teacherData");
         if (!teacherData || teacherData.length == 0) {
             return res.status(402).json({
                 success: false,
@@ -36,21 +39,33 @@ class TeacherController {
             });
         };
 
-        const isPasswordMatched = bcrypt.compare(teacherPassword, teacherData[0]?.teacherPassword as string);
+         // DEBUG: Log password details
+            console.log("Plain Password:", teacherPassword);
+            console.log("Hashed Password:", teacherData[0]?.teacherPassword);
+            console.log("Password Type:", typeof teacherPassword);
+            console.log("Hash Type:", typeof teacherData[0]?.teacherPassword);
+            console.log("incomming email", teacherEmail);
+        console.log("incomming password", teacherPassword);
+        console.log("Password logging hashing", teacherData[0]?.teacherPassword as string)
+
+        const isPasswordMatched = await bcrypt.compare(teacherPassword, teacherData[0]?.teacherPassword as string);
         if (!isPasswordMatched) {
             return res.status(403).json({
                 success: false,
                 message: 'Invalid Credientals'
             });
         };
+        console.log(isPasswordMatched,"isPasswordMatched");
 
         // const token = TokenGenerationService.generateToken( teacherData[0]?.id as string )
-        const token = jwt.sign(teacherData[0]?.id as string, JWT_SECRET,{
-            expiresIn: JWT_EXPIRY
-        });
+        const token = jwt.sign(
+            { id: teacherData[0]?.id as string },
+            JWT_SECRET,
+            { expiresIn: JWT_EXPIRY as string }
+        );
 
         return res.status(200).json({
-            datas: token,
+            datas: { token },
             message: "Teacher Logged in successfully"
         })
 
