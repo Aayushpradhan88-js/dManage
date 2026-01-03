7329200446e1890b6a27f2736e85ae1dc2682c80
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-03 12:03:44 +0545

    making slice for statemanagement

---
+++ client/src/lib/store/user.slice.ts
@@ -0,0 +1,18 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+createSlice({
+    name: "aayush pradhan",
+    initialState: {
+        age: 19,
+        address:"itahari2Sunsari"
+    },
+    reducers: {
+        setAge:{
+
+        },
+
+        setAddress:{
+            
+        }
+    }
+})
