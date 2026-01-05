e4ae056717b7d08a6503b741fe62131874b7ff35
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-05 13:18:32 +0545

    push

--- client/src/lib/store/store.ts
+++ client/src/lib/store/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import exampleSlice from "./slices/exampleSlice";
+import courseSlice from "./slices/courseSlice";
 
 // makeStore is a global variable
 export const makeStore = () => {
     return configureStore({
         reducer: {
-            example: exampleSlice
+            course: courseSlice
         },
     })
 };
+
+type AppDispatch = typeof makeStore.dispatch;
+
+export {AppDispatch}
