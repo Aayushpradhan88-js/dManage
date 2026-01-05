9b1c7902d774e034f98fe4f70b54aa78f56d3c23
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-05 16:16:22 +0545

    fixing error

--- client/src/lib/store/store.ts
+++ client/src/lib/store/store.ts
@@ -2,15 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import courseSlice from "./slices/courseSlice";
 
 // makeStore is a global variable
-export const store = () => {
-    return configureStore({
-        reducer: {
-            course: courseSlice
-        },
-    });
-};
+const store = configureStore({
+    reducer: {
+        course: courseSlice
+    },
+});
 
-type AppDispatch = typeof store.dispatch;
-type RootState = ReturnType<typeof store.getState>;
-
-export {AppDispatch, RootState};
+export default store;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
