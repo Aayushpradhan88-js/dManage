232a94756f51ecd456ddd031aac2dce00a8321bc
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-05 11:49:27 +0545

    courseSice exported and logic completed

--- client/src/lib/store/slices/courseSlice.ts
+++ client/src/lib/store/slices/courseSlice.ts
@@ -1,7 +1,39 @@
+//1. Context setting 
 
-// step (S-1): Redux Setup
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 interface ICourse {
-    id: string | number,
-    title: string,
-    price: number
+    id: string | number;
+    title: string | null;
+    description: string | null;
+    price: number;
+    courseInstructor: string;
 };
+
+interface ICourseState {
+    list: ICourse[];
+    loading: boolean;
+};
+
+const initialState: ICourseState = {
+    list: [],
+    loading: false
+};
+
+const courseSlice = createSlice({
+    name: "Course",
+    initialState: initialState,
+    reducers: {
+        setList: (state, action: PayloadAction<[]>) => {
+            state.list = action.payload;
+        },
+        setLoading: (state, action: PayloadAction<boolean>) => {
+            state.loading = action.payload;
+        },
+    },
+});
+
+const course = courseSlice.actions;
+
+export {course};
+export default courseSlice.reducer;
