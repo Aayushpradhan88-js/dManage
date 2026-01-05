3e9aca7931263b2b49a7d82b7bf2bd012afbaf6e
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-05 17:45:03 +0545

    add comments for future recalling

--- client/src/lib/store/hooks/customHook.ts
+++ client/src/lib/store/hooks/customHook.ts
@@ -3,7 +3,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store";
 
-const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+const useAppDispatch = useDispatch.withTypes<AppDispatch>(); //useDispatch + types = custom hook
 const useApppSelector = useSelector.withTypes<RootState>();
 
 export {useAppDispatch,useApppSelector}
