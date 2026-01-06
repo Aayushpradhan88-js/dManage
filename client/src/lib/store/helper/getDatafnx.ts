9b22553f71c159be241f65c80facb985c953d047
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-06 09:59:46 +0545

    configuring data

--- client/src/lib/store/helper/getDatafnx.ts
+++ client/src/lib/store/helper/getDatafnx.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+// axios.create()          -- valid option
+// axios.create(undefined) -- valid option
 axios.create({
     baseURL: 'http://localhost:4000/api',
-    headers:{
-        "Content-Type":"application/json"
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
     }
 })
