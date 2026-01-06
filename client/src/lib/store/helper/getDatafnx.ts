38a68384aeb6d5d6b9fbeed571ab5d6aeb6e93fb
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-06 11:52:22 +0545

    API fetch code logic completed

--- client/src/lib/store/helper/getDatafnx.ts
+++ client/src/lib/store/helper/getDatafnx.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 
 // axios.create()          -- valid option
 // axios.create(undefined) -- valid option
-axios.create({
+const API = axios.create({
     baseURL: 'http://localhost:4000/api',
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json"
     }
-})
+});
+
+export default API;
