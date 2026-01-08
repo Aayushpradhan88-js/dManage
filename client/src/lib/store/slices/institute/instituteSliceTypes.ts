2f25ecde02bf4a3311e853c62b740f93be126f09
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-08 14:57:02 +0545

    code institute types

---
+++ client/src/lib/store/slices/institute/instituteSliceTypes.ts
@@ -0,0 +1,15 @@
+import { IStatus } from "../../global/types/type"
+
+export interface IInstituteState {
+    instituteName: string,
+    instituteEmail: string,
+    institutePhoneNumber: string,
+    instituteAddress: string,
+    instituteVatNumber: string,
+    institutePanNumber: string,
+};
+
+export interface IInstituteInitialState {
+    institute: IInstituteState,
+    status: IStatus
+};
