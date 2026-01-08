c6a0b3bb96fd747eb3f7259cbf9e063afa49216a
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-08 15:12:59 +0545

    institute completed

---
+++ client/src/lib/store/slices/institute/instituteSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { IInstituteInitialState, IInstituteState } from "./instituteSliceTypes"
+import { IStatus } from "../../global/types/type";
+
+const initialState: IInstituteInitialState = {
+    institute: {
+        instituteName: " ",
+        instituteEmail: " ",
+        institutePhoneNumber: " ",
+        instituteAddress: " ",
+        instituteVatNumber: " ",
+        institutePanNumber: " ",
+    },
+    status: {
+        SUCCESS: "",
+        LOADING: "",
+        ERROR: ""
+    },
+};
+
+const intitituteSlice = createSlice({
+    name: "Institute",
+    initialState: initialState,
+    reducer: {
+        setInstitute: (state: IInstituteInitialState, action: PayloadAction<IInstituteState>) => {
+            state.institute = action.payload;
+        },
+
+        setLoading: (state: IInstituteInitialState, action: PayloadAction<IStatus>) => {
+            state.status = action.payload;
+        },
+    },
+});
+
+export const {setInstitute, setLoading} = intitituteSlice.actions;
+export default intitituteSlice.reducer;
--- client/src/lib/store/store.ts
+++ client/src/lib/store/store.ts
@@ -3,11 +3,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authSlice from "./slices/auth/authSlice"
 import teacherSlice from "./slices/teacher/teacherSlice"
+import intitituteSlice from "./slices/institute/instituteSlice"
 
 // makeStore is a global variable
 const store = configureStore({
     reducer: {
         authSlice: authSlice,
+        institute: intitituteSlice,
         teacher: teacherSlice,
     },
 });
