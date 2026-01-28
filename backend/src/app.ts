7ed465a6822dad16d3ab1a20053b814449383b72
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-28 14:01:23 +0545

    add multiple ports

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,7 +23,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
