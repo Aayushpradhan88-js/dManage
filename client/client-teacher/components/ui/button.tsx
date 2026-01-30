2cd57d5200f0bf24901b4b75a2990b77c853877d
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-30 17:08:36 +0545

    added sidebar

--- client/client-teacher/components/ui/button.tsx
+++ client/client-teacher/components/ui/button.tsx
@@ -19,12 +19,6 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        signupButton :
-          "hover:bg-green-500 cursor-pointer hover:text-black-foreground bg-green-400",
-        watchDemo:
-        "border bg-background shadow-xs hover:bg-slate-700 hover:text-slate-700 cursor-pointer dark:bg-input/30 ",
-        teacherLoginButton:
-          "hover:bg-green-700 cursor-pointer hover:text-black-foreground bg-green-600"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
