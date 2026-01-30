c14068468db267cf767118ecc6655af0030e3f82
Aayush <AAYUSHPRADHAN996@outlook.com>
2026-01-30 12:02:28 +0545

    Writing types

---
+++ client/client-teacher/lib/global/types/types.ts
@@ -0,0 +1,5 @@
+export enum IStatus {
+    SUCCESS = "SUCCESS",
+    LOADING = "LOADING",
+    ERROR = "ERROR"
+};
--- client/client-teacher/lib/store/auth/auth-type.ts
+++ client/client-teacher/lib/store/auth/auth-type.ts
@@ -1,3 +1,12 @@
-interface ITeacherAuth {
-    
-}
+import { IStatus } from "@/lib/global/types/types"
+
+export interface ITeacherAuth {
+    teacherInstitute: string,
+    teacherEmail: string,
+    teacherPassword: string
+};
+
+export interface IInitialTeacherAuth {
+    data: ITeacherAuth[],
+    status: IStatus.LOADING
+};
